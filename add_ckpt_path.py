f53331e0071c8de5cfb17bc29a0cce4bed653f79
Adrien Bousquie <adrien.bousquie@epfl.ch>
2026-05-10 21:36:52 +0200

    fix(path): avoid repeated sys.path churn in data workers

--- add_ckpt_path.py
+++ add_ckpt_path.py
@@ -6,6 +6,8 @@ import os.path as path
 def add_path_to_dust3r(ckpt):
     HERE_PATH = os.path.dirname(os.path.abspath(ckpt))
     # workaround for sibling import
+    if sys.path and sys.path[0] == HERE_PATH:
+        return
     if HERE_PATH in sys.path:
         sys.path.remove(HERE_PATH)
     sys.path.insert(0, HERE_PATH)
